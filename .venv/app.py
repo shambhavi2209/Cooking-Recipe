109e498074c3b032daba0a7fe347aa3ac10d9aab
Shambhavi Singh <iamshruti33@gmail.com>
2024-03-20 20:39:45 +0530

    added carousel images and top pics menu

--- .venv/app.py
+++ .venv/app.py
@@ -1,13 +1,38 @@
-# import flast module
-from flask import Flask
+# import flask module
+from flask import Flask, render_template
  
 # instance of flask application
 app = Flask(__name__)
+
+#sending top pics recipe to the frontend
+RECIPE = [
+    {
+        'id' : 1,
+        'image' : 'butterChicken.jpg',
+        'title' : 'Butter Chicken',
+        'cuisine' : 'Indian Mughlai',
+        'calories' : '230 kcal'
+    },
+    {
+        'id' : 2,
+        'image' : 'khoSuey.jpg',
+        'title' : 'Khao suey',
+        'cuisine' : 'Thai',
+        'calories' : '230 kcal'
+    },
+    {
+        'id' : 3,
+        'image' : 'dumplings.jpg',
+        'title' : 'Dumplings',
+        'cuisine' : 'Pan Asian',
+        'calories' : '230 kcal'
+    }
+]
  
 # home route that returns below text when root url is accessed
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html', recipe=RECIPE)
  
 if __name__ == '__main__':  
    app.run()
